624ba3743a61e65473320b9e937e136ec5346552
grapeJUICE1 <anonymyty123@gmail.com>
2020-10-23 22:16:47 +0600

    Added more game elements

--- Game/Level.py
+++ Game/Level.py
@@ -33,8 +33,8 @@ class Level(GameObject):
         for line in fileinput.input(os.path.join("Assets" , "Levels" , "level"+str(self.__currentLevel)+".dat")):
             for brick in line:
                 if brick == "1":
-                    brick = choice(["Green" , "Red" ,"Purple"])
-                    brick = Brick((x,y) , pygame.transform.scale(pygame.image.load(os.path.join("Assets" , f"{brick}.png")) , GameConstant.BRICK_SIZE) , self.__game , brick)
+                    color = choice(["Green" , "Red" ,"Purple"])
+                    brick = Brick((x,y) , pygame.transform.scale(pygame.image.load(os.path.join("Assets" , f"{color}.png")) , GameConstant.BRICK_SIZE) , self.__game , color)
                     self.__bricks.append(brick)
                     self.__amountOfBricksLeft += 1
 
@@ -46,6 +46,11 @@ class Level(GameObject):
 
                 elif brick == "3":
                     brick = LifeBrick((x,y) , pygame.transform.scale(pygame.image.load(GameConstant.SPRITE_LIFE_BRICK) , GameConstant.BRICK_SIZE) , self.__game)
+                    self.__bricks.append(brick)
+                    self.__amountOfBricksLeft += 1
+                elif brick == "4":
+                    color = choice(["small-green" ,  "small-purple" , "small-red" , "small-blue"])
+                    brick = LifeBrick((x,y) , pygame.transform.scale(pygame.image.load(os.path.join("Assets" , f"{color}.png")) , GameConstant.BRICK_SIZE) , self.__game)
                     self.__bricks.append(brick)
                     self.__amountOfBricksLeft += 1
 
